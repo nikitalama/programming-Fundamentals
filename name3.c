8dacfb8c201f85422a209c41bc1e6e101af99789
nikitalama <nikita.lama555@gmail.com>
2016-11-14 12:44:00 +0545

    Add files via upload

---
+++ name3.c
@@ -0,0 +1,19 @@
+\*
+student name:nikita lama
+subject     :programming fundamental
+roll no     :09
+lab no      :01
+program     :write a c program to print name ask from user.
+date        :nov 14,2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+int main() {
+char name[20] ;
+printf("what is your name\n");
+scanf("%s",&name);
+printf("name of user is %s",name);
+getch();
+return 0;
+}
