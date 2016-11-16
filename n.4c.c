9d7e1924ae076df62687a37b39844362bf17b0bd
nikitalama <nikita.lama555@gmail.com>
2016-11-15 20:59:23 -0800

    Add files via upload

---
+++ n.4c.c
@@ -0,0 +1,22 @@
+/*
+student name:nikita lama
+subject     :programming fundamental
+roll no     :09
+lab no      :02
+program     :write a c program to print sum of two numbers asked from users.
+date        :nov 16,2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main(){
+int n1,n2,sum;
+printf("enter the first number\n");
+scanf("%d",&n1);
+printf("enter the second number\n");
+scanf("%d",&n2);
+sum=n1+n2;
+printf("sum of two numbers is %d",sum);
+getch();
+return 0;
+}
+
