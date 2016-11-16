4668ea9befc2cd187aed388b7f383142394e0e45
nikitalama <nikita.lama555@gmail.com>
2016-11-15 21:15:03 -0800

    Add files via upload

---
+++ 1n.c
@@ -0,0 +1,15 @@
+/*
+student name:nikita lama
+subject     :programming fundamental
+roll no     :09
+lab no      :01
+program     :write a c program to print your name.
+date        :nov14,2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main(){
+printf("nikita");
+getch();
+return 0;
+}
