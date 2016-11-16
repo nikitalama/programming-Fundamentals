82f48fe8a3f3bdde6e873a610639673ab75360cf
nikitalama <nikita.lama555@gmail.com>
2016-11-15 22:39:44 -0800

    Add files via upload

---
+++ divide.n.c
@@ -0,0 +1,22 @@
+/*
+student name:nikita lama
+subject     :programming fundamental
+roll no     :09
+lab no      :02
+program     :write a c program to print divide of two numbers asked from users
+date        :nov16,2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main(){
+float a,b,divide;
+printf("enter first number");
+scanf("%f",&a);
+printf("enter second number");
+scanf("%f",&b);
+divide=a/b;
+printf("divide of two numbers is %f",divide);
+getch();
+return 0;
+}
+
