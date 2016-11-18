3960bf139cb5403febc25d679831ffe2e31c6488
nikitalama <nikita.lama555@gmail.com>
2016-11-18 12:37:05 +0545

    Add files via upload

---
+++ trapezium.c
@@ -0,0 +1,44 @@
+/*
+student name :nikita lama
+subject      :programming fundamental
+roll no      :09
+lab no       :03
+program      :write a c program to print area of trapezium.
+date         :nov 18,2016
+*/
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+float a,b,h,area;
+printf("enter the first length\n");
+scanf("%f",&a);
+printf("enter the second length\n");
+scanf("%f",&b);
+prinf("enter the height\n");
+scanf("%f",&h);
+area=((a+b)*h)/2;
+printf("area of trapezium is %f\n",area);
+getch();
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
