276e078084990ed220584cd0c5e793b36bb11314
nikitalama <nikita.lama555@gmail.com>
2016-11-23 12:37:41 +0545

    Add files via upload

---
+++ ascii value.c
@@ -0,0 +1,17 @@
+/*Name:nikita lama
+Subject:programming fundamental
+Program:Write a progarm to find the ASCII value
+Roll No:09
+Lab No:
+Date:
+*/
+#include<stdio.h>
+#include<conio.h>
+int main(){
+char character;
+printf("enter character\n");
+scanf("%c",&character);
+printf("ASCII value of character is:%d\n",character);
+getch();
+return 0;
+}
