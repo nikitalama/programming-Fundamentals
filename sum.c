2d0d72415a3decd33aa864f0dc1c1fbc8724dbfc
nikitalama <nikita.lama555@gmail.com>
2016-11-23 10:42:53 +0545

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,22 @@
+/*
+student name:nikita lama
+subject     :programming fundamental 
+roll no     :09
+lab no      :05
+program     :write a program to find the sum  of three number 
+date        : 23 nov,2016
+*/
+#include<stdio.h>
+int main()
+{
+int a,b,c,sum;
+printf("enter the  first number:\n");
+scanf("%d",&a);
+printf("enter the  second number:\n");
+scanf("%d",&b);
+printf("enter the  third number:\n");
+scanf("%d",&c);
+sum=a+b+c;
+printf("sum of three number is:%d\n",sum);
+return 0;
+}
