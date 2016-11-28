cb194570f3f84afd8325a7861898e46fc14093c4
nikitalama <nikita.lama555@gmail.com>
2016-11-28 12:49:55 +0545

    Add files via upload

---
+++ square.c
@@ -0,0 +1,25 @@
+/*Name:Sabina Prajapati
+Subject:Programming Fundamental
+Program:Write a program to print area and volume of the sphere
+Roll no:
+Lab No:3
+Date:16thNov,2016*/
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main(){
+    int x,square;
+    printf("enter the value of x\n");
+    scanf("%d",&x);
+    square=pow(x,2);
+    printf("the square of the number is %d",square);
+
+    getch();
+    return 0;
+
+
+
+
+
+
+}
