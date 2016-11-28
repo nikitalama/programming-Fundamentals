522f5e1140e9a7578767054a3598bf1d01105583
nikitalama <nikita.lama555@gmail.com>
2016-11-28 12:50:13 +0545

    Add files via upload

---
+++ sq roots.c
@@ -0,0 +1,18 @@
+/*Name:Sabina Prajapati
+Subject:Programming Fundamental
+Program:Write a program to enetr any number and calculate its square
+Roll no:
+Lab No:3
+Date:16thNov,2016*/
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main(){
+    int x,sr;
+    printf("enter the value of x\n");
+    scanf("%d",&x);
+    sr=sqrt(x);
+    printf("the square roots of the number is %d",sr);
+    getch();
+    return 0;
+}
