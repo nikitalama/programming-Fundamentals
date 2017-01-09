9fb516b627a2164c0d7bb0baa8b2ee750e401224
nikitalama <nikita.lama555@gmail.com>
2017-01-09 18:29:19 +0545

    Update inverse.c

--- inverse.c
+++ inverse.c
@@ -3,7 +3,7 @@ student name:nikita lama
 subject     : programming fundamental
 roll no     :
 lab no      :16
-program     :write a c program to print all natural number from 1 to n using while loop
+program     :write a c program to print all natural number in reverse from (n to 1)using while loop.
 date        :8 jan,2016
 */
 #include<stdio.h>
