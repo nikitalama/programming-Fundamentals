67c169b4f198b27de9e12a88c87e1d101e84a958
nikitalama <nikita.lama555@gmail.com>
2017-01-12 11:01:05 +0545

    Add files via upload

---
+++ table.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	int a,b,multiplication;
+	
+	for(a=1;a<=2;a++)
+	{
+		for(b=1;b<=12;b++)
+		{
+			multiplication=a*b;
+			printf("%d",multiplication);
+		}
+		printf("\n");
+	}
+		return 0;
+	}
