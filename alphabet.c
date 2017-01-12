efb4eba6406cdc70b9f383f6d82b9719b7b84917
nikitalama <nikita.lama555@gmail.com>
2017-01-12 11:00:02 +0545

    Add files via upload

--- alphabet.c
+++ alphabet.c
@@ -1,26 +1,22 @@
+
 /*
 student name:nikita lama
 subject     :programming fundamental
-roll no     :20
-lab no      :14
-program     :write a c program to check character assigned from user is alphabet or not
-date        :21 dec,2016
+roll no     :
+lab no      :17
+program     :write a c program to print all alphabets from a to z using for, while and do while loop.
+date        :12 jan,2017
 */
 #include<stdio.h>
 int main(){
-	char ch;
-	int ASCII;
-	printf("Enter any character\n");
-	scanf("%c",&ch);
-	ASCII=ch;
-	if((ASCII>=65&&ASCII<=90)||(ASCII>=97&&ASCII<=122))
-	{
-		printf("it is an alphabet");
-	}
-	else
-	{
-		printf("it is not alphabet");
-	}
-	return 0;
+char ch='a';
+int i;
+for(i=1;i<=26;i++)
+{
+printf("%c\t",ch);
+ch++;
+
+
 }
-	     
+return 0;
+}
